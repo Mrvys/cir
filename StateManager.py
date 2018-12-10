b434369efce9a46bbfe016aa09ab2d927aafa22c
Bartool <bartulizna@gmail.com>
2018-12-10 14:03:06 +0100

    Added last and most popular orders for person. Bug fixes

--- StateManager.py
+++ StateManager.py
@@ -6,13 +6,14 @@ import random
 
 class StateManager:
 
+    __question_prefix = "Would you like "
+    __epsilon = 0.0  # TODO How and when should we decrease it?
+    __learning_rate = 0.9  # TODO Fit the best
+    gamma = 0.95  # discount rate
+
     def __init__(self, gender, group):
         self.__gender = gender
         self.__group = group
-
-        self.__epsilon = 0.5  # TODO How and when should we decrease it?
-        self._learning_rate = 0.9  # TODO Fit the best
-        self.gamma = 0.95  # discount rate
 
         self.__state_loader = StateLoader("./universalUserActions.json")
 
@@ -23,7 +24,9 @@ class StateManager:
         self.__previous_state_id = "A"
         self.__qtable = self.__state_loader.load_qtable(gender, group, self.__states)
 
-        self.__last_question = ""
+        self.__last_order = ""
+        self.__orders = {}
+        self.__last_questions = []
 
         self.__text_recognizer = TextRecognizer()
 
@@ -59,7 +62,10 @@ class StateManager:
 
         self.__previous_state_id = self.__current_state_id
         self.__current_state_id = self.get_current_state().get_next_state(chosen_action)
-        self.__last_question = chosen_action
+
+        self.__last_questions.append(chosen_action)
+        if len(self.__last_questions) > 3:
+            self.__last_questions = self.__last_questions[1:]
 
         return chosen_action + '?'
 
@@ -68,7 +74,7 @@ class StateManager:
         random_action = random.randint(0, len(actions_available) - 1)
         action_chosen = actions_available[random_action]
 
-        if action_chosen == self.__last_question:
+        if action_chosen in self.__last_questions:
             return self.get_random_action()
 
         return action_chosen
@@ -78,6 +84,13 @@ class StateManager:
 
     def finished(self):
         if self.get_current_state().is_final():
+            self.__last_order = self.get_current_state().get_name()
+
+            if self.__last_order in self.__orders:
+                self.__orders[self.__last_order] += 1
+            else:
+                self.__orders[self.__last_order] = 1
+
             self.save_qtable()
 
             drink_name = self.get_current_state().get_name()
@@ -88,11 +101,28 @@ class StateManager:
 
         return False
 
+    def get_most_popular_order_actions(self):
+        max_value = 0
+        max_orders = []
+        first = True
+        for order, value in self.__orders.items():
+            if first:
+                first = False
+                max_value = value
+                max_orders = [self.__question_prefix + order]
+            elif value > max_value:
+                max_orders = [self.__question_prefix + order]
+                max_value = value
+            elif value == max_value:
+                max_orders.append(self.__question_prefix + order)
+
+        return max_orders
+
     def update_qtable(self, action_chosen):
         current_q = self.__qtable[self.__current_state_id][action_chosen]
         reward = self.get_current_state().get_reward(action_chosen)
         next_state_id = self.get_current_state().get_next_state(action_chosen)
-        delta_q = self._learning_rate * (reward + self.gamma * self.get_max_q_value(next_state_id) - current_q)
+        delta_q = self.__learning_rate * (reward + self.gamma * self.get_max_q_value(next_state_id) - current_q)
 
         self.__qtable[self.__current_state_id][action_chosen] = current_q + delta_q
 
@@ -102,12 +132,14 @@ class StateManager:
         first = True
 
         for action, value in self.__qtable[self.__current_state_id].items():
+            value += self.get_bias(action, value)
+
+            if action in self.__last_questions:
+                continue
             if first:
                 first = False
                 max_value = value
                 max_actions = [action]
-            if action == self.__last_question:
-                continue
             if value > max_value:
                 max_actions = [action]
                 max_value = value
@@ -130,4 +162,15 @@ class StateManager:
     def restart(self):
         self.__current_state_id = "A"
         self.__previous_state_id = "A"
-        self.__last_question = ""
+        self.__last_questions = []
+
+    def get_bias(self, action, value):
+        bias = 0
+
+        if action == self.__question_prefix + self.__last_order:
+            bias += abs(value) * 0.5
+
+        if action in self.get_most_popular_order_actions():
+            bias += abs(value) * 0.5
+
+        return bias
