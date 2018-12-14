4f4a6d11c865ad8afcd2a56447db59816bdc5b95
Mrvys <mrvy13@gmail.com>
2018-12-14 19:36:25 +0100

    text recognizer

--- TextRecognizer.py
+++ TextRecognizer.py
@@ -14,10 +14,26 @@ class TextRecognizer:
             user_input = user_input + ' wine'
             return user_input.lower()
 
+        # Determine semantic similarities
+        doc1 = self.__nlp(user_input)
+        top_score =	{
+            'score': 0,
+            'input': None
+        }
+
         for action in actions:
+            doc2 = self.__nlp(action)
+            similarity = doc1.similarity(doc2)
+            #print(doc1.text, doc2.text, similarity)
             if action in user_input:
                 return action
+            elif top_score['score'] < similarity:
+                top_score['score'] = similarity
+                top_score['input'] = action
 
+        if top_score['score'] > 0.6:
+            #print(top_score['input'])
+            return top_score['input']
         return -1
 
         # TODO Change to choose the most similar phrase
