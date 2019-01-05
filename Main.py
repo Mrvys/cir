a6b9dabc2fa8497d31053853f74c542f5800e973
Mrvys <mrvy13@gmail.com>
2019-01-05 23:42:34 +0100

    text to speech in Main.py

--- Main.py
+++ Main.py
@@ -2,12 +2,16 @@ from StateManager import StateManager
 from listen import Listen
 import time
 from speech import Speech
+from audio import Audio
+import pygame as pg
+import os
 
 finish = False
 
 state_manager = StateManager("man", "student")
 listen = Listen()
 speech = Speech()
+audio = Audio()
 
 while not finish:
     state_manager.restart()
@@ -17,15 +21,14 @@ while not finish:
         if response != -1:
             question = state_manager.choose_question()
         print(question)
+        audio.audio(question)
 
-        user_input = input('Enter your input:')
+        #user_input = input('Enter your input:')
 
         user_input = listen.listen()
-        #time.sleep(2)
         #user_input = speech.detect()
         print(user_input)
-
-        response = state_manager.process_input(user_input)
+        state_manager.process_input(user_input)
 
     user_input = input('Press enter to make a new order or insert 1 to finish:')
     if user_input == "1":
---
+++ audio.py
@@ -0,0 +1,45 @@
+from google.cloud import texttospeech
+import pygame as pg
+
+class Audio:
+
+	def audio(self, text_to_read):
+		# Instantiates a client
+		client = texttospeech.TextToSpeechClient()
+
+		self.__text_to_read = text_to_read
+
+		# Set the text input to be synthesized
+		synthesis_input = texttospeech.types.SynthesisInput(text=self.__text_to_read)
+
+		# Build the voice request, select the language code ("en-US") and the ssml
+		# voice gender ("neutral")
+		voice = texttospeech.types.VoiceSelectionParams(
+		    language_code='en-US',
+		    name='en-US-Wavenet-D')
+
+		# Select the type of audio file you want returned
+		audio_config = texttospeech.types.AudioConfig(
+		    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+		# Perform the text-to-speech request on the text input with the selected
+		# voice parameters and audio file type
+		response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+		# The response's audio_content is binary.
+		with open('resources\\output.mp3', 'w+b') as out:
+		    # Write the response to the output file.
+		    out.write(response.audio_content)
+
+		pg.mixer.init()
+		clock = pg.time.Clock()
+		f = open('resources\\output.mp3', 'rb')
+		pg.mixer.music.load(f)
+		#pg.mixer.music.load('resources\\output.mp3')
+		pg.mixer.music.play(loops=0, start=0.0)
+		while pg.mixer.music.get_busy():
+			# check if playback has finished
+			clock.tick(30)
+		pg.mixer.music.stop()
+		pg.mixer.quit()
+		f.close()
